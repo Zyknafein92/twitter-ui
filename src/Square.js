805d12d6fe4e4f70e5a6d77dd4bc334ba7da0d0d
Zyk <deneux.j@gmail.com>
2019-06-27 16:11:06 +0200

    Implement history

--- src/Square.js
+++ src/Square.js
@@ -8,16 +8,4 @@ function Square(props) {
     );
 }
 
-/*class Square extends React.Component {
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick()}>
-                {this.props.value}
-            </button>
-        )
-    }
-}*/
-
 export default Square;
